651d057e2630f2529309ec8aca61c58da5f8b9d6
josueben <josuebenbeatle@outlook.com>
2018-05-24 12:41:49 -0500

    Tracking module Josue Mendez 24/05/2018

--- src/app/modules/tracking/tracking.component.ts
+++ src/app/modules/tracking/tracking.component.ts
@@ -10,6 +10,32 @@ export class TrackingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  goEstafeta(){
+    let url:string = "http://www.estafeta.com/Rastreo/";
+    window.open(url, "_blank");
+  }
+
+  goFedEx(){
+    let url:string = "https://www.fedex.com/apps/fedextrack/?action=track&cntry_code=mx";
+    window.open(url, "_blank");
+  }
+
+  goRedPack(){
+    let url:string = "http://www.redpack.com.mx/rastreo-de-envios/";
+    window.open(url, "_blank");
+  }
+
+  goDHL(){
+    let url:string = "http://www.dhl.com.mx/es/express/rastreo/monitorear_envios.html";
+    window.open(url, "_blank");
+  }
+
+  goPaquetexpress(){
+    let url:string = "https://www.paquetexpress.com.mx/";
+    window.open(url, "_blank");
   }
 
 }
